d2239dc05f15201aa345ac3a877cc8d4533b481c
yaodev778 <davidruizy1983@yandex.com>
2018-09-24 21:36:26 +0800

    combine reducer modified

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import { pendingTasksReducer } from 'react-redux-spinner'
+import { penderReducer } from 'redux-pender'
 import app from './app'
 import login from './login'
 import auth from './auth'
@@ -8,7 +9,28 @@ import auth from './auth'
 export default combineReducers({
   routing: routerReducer,
   pendingTasks: pendingTasksReducer,
+  pender: penderReducer,
   app,
   login,
   auth
 })
+
+
+/* To change to this module for the future
+// imports all file except index.js
+const req = require.context('.', true, /^(?!.\/index)(?!.\/__tests__).*.js$/);
+
+const modules = { };
+
+req.keys().forEach((key) => {
+  const regex = /.\/(.*?).js$/;
+  const moduleName = regex.test(key) && key.match(regex)[1];
+  modules[moduleName] = req(key).default;
+});
+
+modules.routing = routerReducer
+modules.pendingTasks = pendingTasksReducer
+modules.pender = penderReducer
+
+export default combineReducers(modules);
+*/
